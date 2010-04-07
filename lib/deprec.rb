6c6d9828be6ee1d6a6363a74d080186c030a856d
Michael Kovacs <kovacs@gmail.com>
2010-04-07 07:49:58 -0700

    update puts

--- lib/deprec.rb
+++ lib/deprec.rb
@@ -1,3 +1,6 @@
+puts ''
+puts '=-=-=-=-=- Using CableCarSoftware version of deprec =-=-=-=-=-'
+puts ''
 unless Capistrano::Configuration.respond_to?(:instance)
   abort "deprec2 requires Capistrano 2"
 end
@@ -5,4 +8,3 @@ end
 require "#{File.dirname(__FILE__)}/deprec/capistrano_extensions"
 require "#{File.dirname(__FILE__)}/vmbuilder_plugins/all"
 require "#{File.dirname(__FILE__)}/deprec/recipes"
-
